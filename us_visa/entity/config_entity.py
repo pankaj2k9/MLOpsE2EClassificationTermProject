6f20e9d7e477ca32227dd88d3f9c08755987f03c
pankaj2k9 <pkp2.me2k9@gmail.com>
2025-11-21 03:02:48 +0600

    Implement data transformation and model training components in the pipeline

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -31,7 +31,7 @@ class DataValidationConfig:
     data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
     drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
-    
+
 
 
 
@@ -45,7 +45,7 @@ class DataTransformationConfig:
     transformed_object_file_path: str = os.path.join(data_transformation_dir,
                                                      DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
                                                      PREPROCSSING_OBJECT_FILE_NAME)
-    
+
 
 
 @dataclass
